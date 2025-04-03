60748e330331f892e4a8ba37484e54367fc20de4
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-03 16:13:22 -0500

    refactor: enhance dashboard item structure and improve component imports

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { StatusComponent } from './status/status.component';
-import { TrafficComponent } from './traffic/traffic.component';
-import { TicketsComponent } from './tickets/tickets.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { StatusComponent } from './dashboard/server-status/status.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, StatusComponent, TrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, StatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  
   
 }
